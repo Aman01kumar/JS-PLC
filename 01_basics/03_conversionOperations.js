bd8459f64bb2da784343fc004cab05443ce84623
Aman01kumar <sammar4568@gmail.com>
2025-05-12 06:45:00 +0000

    conversions of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,33 @@
+let score = true
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+// null => 0
+// undefined = undefined
+
+let loggedIn = "aman"
+
+let isLoggedIn = Boolean(loggedIn)
+
+// console.log(isLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false
+// "aman" => true
+
+let number = 33
+let stringNumber = String(number)
+console.log(stringNumber);
+
+
+
+
+
