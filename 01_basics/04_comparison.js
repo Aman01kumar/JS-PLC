3441afe4a84a0a1b235dffd5002e300c4980cc1d
Aman01kumar <sammar4568@gmail.com>
2025-05-12 07:19:07 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); // comparison convert null into a number, thats why null >= 0 is true, while == remain same 
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+
+// strict check
+
+console.log("2" === 2);
+console.log("2" == 2);
+
+
+
+
+
+
+
+
