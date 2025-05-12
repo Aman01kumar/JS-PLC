32bf8de277f0fa05704af3e35c76fdc04b98935e
Aman01kumar <sammar4568@gmail.com>
2025-05-12 07:00:11 +0000

    opeartions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -25,7 +25,45 @@ let isLoggedIn = Boolean(loggedIn)
 
 let number = 33
 let stringNumber = String(number)
-console.log(stringNumber);
+// console.log(stringNumber);
+
+// ***************Operations*****************************
+
+let value = 3;
+let negVlalue = -value
+// console.log(negVlalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello "
+let str2 = "aman"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 / 2);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
 
 
 
