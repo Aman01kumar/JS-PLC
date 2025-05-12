81a56230256a5041de693ed13947b3baaf9c34d4
Aman01kumar <sammar4568@gmail.com>
2025-05-12 06:28:28 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict" // treat all js code on newer version
+
+// number = 2 to the power 51
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined = 
+// symbol = unique
+// object
+
+console.log(typeof null); // type object
+console.log(typeof undefined); // type undefined
+
