7ccf979ef5a4591f9d3db8ce0d7cccb157cceaf8
Aman01kumar <sammar4568@gmail.com>
2025-05-12 09:50:54 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,43 @@
+const name = "Aman"
+const repoCount = 10
+
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello, my name is: ${name}, and my repo count is: ${repoCount}`); // this is called string interpolation
+
+const gameName = new String('aman-kumar-com')
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('n'));
+
+// const newString = gameName.substring(0, 4) // cannot give negative start value
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const Name = "    aman    "
+console.log(Name);
+console.log(Name.trim());
+
+const url = "https://aman.com/aman%20kumar"
+console.log(url.replace("%20", "-"))
+console.log(url.includes('aman'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
