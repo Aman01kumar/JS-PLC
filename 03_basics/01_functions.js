e847a483eb10a3403406915be4ff12498b22050c
Aman01kumar <sammar4568@gmail.com>
2025-05-17 06:19:42 +0000

    function s properties

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,8 +32,41 @@ function loggeinUserMessage(username = "sam"){
 }
 
 // console.log(loggeinUserMessage("Aman"))
-console.log((loggeinUserMessage("")));
-console.log(loggeinUserMessage());
-console.log(loggeinUserMessage("Aman"));
+// console.log((loggeinUserMessage("")));
+// console.log(loggeinUserMessage());
+// console.log(loggeinUserMessage("Aman"));
+
+
+// ***********************************************************************************************
+
+function calculatePrice(val1, val2,...num1){ // rest operator
+    return num1
+}
+// console.log(calculatePrice(200, 400, 600, 700));
+
+const user = {
+    name: "Aman",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.name} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+// handleObject({
+//     name: "sam",
+//     price: 399
+// })
+
+const mynewArray = [100, 200, 400, 600]
+
+function returnSecondValue(anyArray){
+    return anyArray[1]
+}
+
+console.log(returnSecondValue(mynewArray))
+console.log(returnSecondValue([100, 400, 300, 700]));
+
+
 
 
