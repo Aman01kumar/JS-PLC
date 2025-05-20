a30772764204c81756a0b40d82d88f741fdb8736
Aman01kumar <sammar4568@gmail.com>
2025-05-20 11:26:25 +0000

    while, dowhile

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// While 
+
+let i = 0;
+while(i <= 10){
+    // console.log(`value of i is ${i}`)
+    i = i+2
+}
+
+let myArray = ["Batman", "Superman", "Flash"]
+let idx = 0
+while(idx < myArray.length){
+    // console.log(`value is ${myArray[idx]}`)
+    idx++
+}
+
+let score = 100
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score <= 10);
